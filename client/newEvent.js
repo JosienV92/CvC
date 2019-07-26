516b5838b51ec2ec7c2ffd47fe061f8c6522fe79
Hadewych1362 <32568235+Hadewych1362@users.noreply.github.com>
2019-07-26 13:47:33 +0200

    updated all

--- client/newEvent.js
+++ client/newEvent.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import DayPicker from 'react-day-picker';
-import 'react-day-picker/lib/style.css';
-
 Template.newEvent.helpers({
-  DayPicker() {
-    return DayPicker;
-  },
+
 });
